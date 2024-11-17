e631e1742556c9843308670f2b994e9fef7c9af6
Aniket Rajesh Kolte <143741415+Aniketkolte2@users.noreply.github.com>
2024-11-17 14:37:40 +0530

    Add files via upload

---
+++ PORT SCANNER.py
@@ -0,0 +1,23 @@
+import pyfiglet
+import sys
+import socket
+from datetime import datetime
+
+ascii_banner = pyfiglet.figlet_format("PORT SCANNER")
+print(ascii_banner)
+
+
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.setdefaulttimeout(3)
+host = input("Enter your target IP :")
+
+def portscanner(port):
+    if socket.connect_ex((host , port)):
+        print("port %d is closed" % (port))
+        
+    else:
+        print("port %d is open" % (port))
+        
+for port in range(1,1000):
+    portscanner(port)
