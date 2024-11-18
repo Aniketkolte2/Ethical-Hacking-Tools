55e3053cf7a64962ad355ae400f916f433bdb463
Aniket Rajesh Kolte <143741415+Aniketkolte2@users.noreply.github.com>
2024-11-18 12:11:26 +0530

    Update PORT SCANNER.py

--- PORT SCANNER.py
+++ PORT SCANNER.py
@@ -1,5 +1,5 @@
+
 import pyfiglet
-import sys
 import socket
 from datetime import datetime
 
@@ -12,14 +12,25 @@ target = input("Enter Target IP: ")
 x = int(input("Enter starting port: "))
 y = int(input("Enter last port: "))
 
+if x < 0 or y < 0 or x > 65535 or y > 65535 or x > y:
+        print("Invalid port range. Ports must be between 0 and 65535")
+        exit()
+
 # Iterating through the port range
 for port in range(x, y + 1):  # Adding +1 to include the last port
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.settimeout(3)  # Timeout for each connection
-    result = sock.connect_ex((target, port))  # Connect to the port
+    try:
+       sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+       sock.settimeout(3)  # Timeout for each connection
+       result = sock.connect_ex((target, port))  # Connect to the port
     
-    if result == 0:
-        print(f"Port {port} is open")  # f-string to include the variable
-  
-    sock.close()  # Closing the socket
-
+       if result == 0:
+           print(f"Port {port} is open")  # f-string to include the variable
+    
+       else:
+            print(f"Port {port} is closed")
+             
+    except socket.error as e:
+        print(f"Socket error on port {port}: {e}")
+        
+    finally:
+           sock.close()  # Closing the socket
